c31eca0cad9336b5776d68ecc08e62501069c5fa
unknown <egkirpichev@gmail.com>
2022-07-17 16:42:11 +0300

    feat(country-list): add component stylization

--- src/components/CountryItem.tsx
+++ src/components/CountryItem.tsx
@@ -7,11 +7,11 @@ interface IProps {
 
 export const CountryItem = ({country}: IProps) => {
 	return (
-		<li className='list-group-item d-flex'>
-			<img className='' src={country.flag} alt="country_flag" />
-			<p>{country.name}</p>
-			<p>{country.capital}</p>
-			<p>{country.area}</p>
+		<li className='list-group-item d-flex justify-content-between align-items-center'>
+			<img className='col-1' src={country.flag} alt="country_flag" />
+			<p className='m-0'>{country.name}</p>
+			<p className='m-0'>{country.capital}</p>
+			<p className='m-0'>{country.area}</p>
 		</li>
 	)
 }
