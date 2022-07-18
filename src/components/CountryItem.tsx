52c3b3bed759d2b223af0b901f48acc9d63b332b
unknown <egkirpichev@gmail.com>
2022-07-18 21:20:25 +0300

    feat(country-list): add keys to country list items

--- src/components/CountryItem.tsx
+++ src/components/CountryItem.tsx
@@ -7,7 +7,7 @@ interface IProps {
 
 const CountryItem = ({ country }: IProps) => {
   return (
-    <li className="list-group-item d-flex justify-content-between align-items-center">
+    <li key={country.name} className="list-group-item d-flex justify-content-between align-items-center">
       <img className="col-1" src={country.flag} alt="country_flag" />
       <p className="m-0 col-1">{country.name}</p>
       <p className="m-0 col-1">{country.capital}</p>
